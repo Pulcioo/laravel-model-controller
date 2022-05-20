a8d13a60e1105c5739066a8e00bdfdd992c66940
Davide Pulci <davide.pulci@gmail.com>
2022-05-20 16:58:13 +0200

    problemi con la connessione al db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Movie;
+
 class PageController extends Controller
 {
     //
 
     public function index(){
+
+        $movies = Movie::all();
+
         return view('page');
     }
 }
